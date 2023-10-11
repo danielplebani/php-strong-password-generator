ed1a0446be8712ead42f441d538f89bac8d04e43
danielplebani <danielpleba03@gmail.com>
2023-10-11 16:20:17 +0200

    Milestone 1
    Create a form that sends the password length in GET. One of our functions will use this data to generate a random password (made up of letters, capital letters, numbers and symbols) to return to the user. We write everything (logic and layout) in a single index.php file

---
+++ index.php
@@ -0,0 +1,96 @@
+<!-- 
+Descrizione
+Dobbiamo creare una pagina che permetta ai nostri utenti di utilizzare il nostro generatore di password (abbastanza) sicure. L’esercizio è suddiviso in varie milestone ed è molto importante svilupparle in modo ordinato.
+
+Milestone 1
+Creare un form che invii in GET la lunghezza della password. Una nostra funzione utilizzerà questo dato per generare una password casuale (composta da lettere, lettere maiuscole, numeri e simboli) da restituire all’utente. Scriviamo tutto (logica e layout) in un unico file index.php
+
+
+-->
+
+
+<?php
+
+
+$length = $_GET['length'];
+//var_dump($length);
+
+
+?>
+
+
+
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>password</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!--bootstrap-css-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+
+</head>
+
+<body style="background-color: #011633;">
+
+    <main class="container py-5 text-center text-white">
+        <h1 class="text-secondary">Strong Password Generator</h1>
+        <h2>Genera una password sicura</h2>
+
+        <div style="border-radius: 5px;" class="bg-primary-subtle p-3 text-secondary text-start mb-3">Nessun paramentro inserito</div>
+
+        <form style="border-radius: 5px; padding-right: 20%" action="" method="get" class="py-5 ps-5 bg-light text-black text-start">
+            <div class="d-flex justify-content-between mb-5">
+                <span>Lunghezza password:</span>
+                <input style="border-radius: 5px; border-color:grey" type="number" name="length" id="length" placeholder="inserisci la lunghezza..." class="py-2">
+            </div>
+
+            <div class="d-flex justify-content-between mb-3">
+                <span>Consenti ripetizioni di più caratteri:</span>
+                <div style="margin-right: 7rem;" class="d-flex flex-column">
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="repetitions" id="repetitions">
+                        <label class="form-check-label" for="repetitions">
+                            Sì
+                        </label>
+                    </div>
+                    <div class="form-check mb-5">
+                        <input class="form-check-input" type="radio" name="no_repetitions" id="no_repetitions" checked>
+                        <label class="form-check-label" for="no_repetitions">
+                            No
+                        </label>
+                    </div>
+
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="letters" name="letters" checked>
+                        <label class="form-check-label" for="letters">
+                            Lettere
+                        </label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="numbers" name="numbers" checked>
+                        <label class="form-check-label" for="numbers">
+                            Numeri
+                        </label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="symbols" name="symbols" checked>
+                        <label class="form-check-label" for="symbols">
+                            Simboli
+                        </label>
+                    </div>
+                </div>
+            </div>
+
+            <button type="submit" class="btn btn-primary">Invia</button>
+            <button type="reset" class="btn btn-secondary">Annulla</button>
+        </form>
+    </main>
+
+</body>
+
+</html>
